68a3bfe0ddcd0c434ed87a56a97d2a3626c3ee28
Mariem Mallek <mariemmallek1@gmail.com>
2025-06-10 00:29:22 +0100

    sécurité BD

--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 module.exports.connectToMongoDB = async()=>{
     mongoose.set("strictQuery",false)  //Pour les injections
-    mongoose.connect("mongodb://localhost:27017/SantePlus").then(
+    mongoose.connect(process.env.UrlDB).then(
         ()=>{
             console.log("connection à ma BD ")
         }
